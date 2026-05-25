f2b11bcd7586ee7227279bc70f1cba3d9bab3f26
Enrico Stemmer <enrico@h3rmt.dev>
2026-05-25 23:26:52 +0200

    fix: fix package installs

--- build_files/files/install-packages.sh
+++ build_files/files/install-packages.sh
@@ -8,8 +8,12 @@ log() {
   echo "[install-packages.sh] $*"
 }
 
+/prepare/files/install-packages.sh: line 26: pacman --noconfirm -Sy: command not found
+
 if [[ -z "$package_file" || -z "$package_cmd" ]]; then
 	echo "Usage: $0 <package-file> <command>" >&2
+	echo "Example: $0 /opt/after-boot/flatpak-apps.conf flatpak install -y --noninteractive" >&2
+	echo "Example: $0 /prepare/files/programs.conf pacman --noconfirm -Sy" >&2
 	exit 1
 fi
 
