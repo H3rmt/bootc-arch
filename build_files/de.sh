f90fc584564abe99fc7567e1a19e617dd38ac9a7
H3rmt <enrico@h3rmt.dev>
2026-07-08 21:09:39 +0200

    add more packages

--- build_files/de.sh
+++ build_files/de.sh
@@ -4,10 +4,10 @@ set -xeuo pipefail
 # basic gui programs
 /prepare/files/install-packages.sh "/prepare/files/programs.conf" pacman --noconfirm -S
 
-# Aur Packages
-su builder -c '/prepare/files/install-packages.sh "/prepare/files/aur-packages.conf" yay --noconfirm --needed -S'
-
 # Hyprland
 /prepare/files/install-packages.sh "/prepare/files/hyprland-files.conf" pacman --noconfirm -S
+
+# Aur Packages
+su builder -c '/prepare/files/install-packages.sh "/prepare/files/aur-packages.conf" yay --noconfirm --needed -S'
 
 systemctl enable sddm.service
